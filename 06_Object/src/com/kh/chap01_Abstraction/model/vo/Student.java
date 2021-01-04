7e87745fd3bf250bc14d6fe9722ee3a2650f35ad
Hye Jeong <hye.93@hotmail.com>
2021-01-05 00:45:00 +0900

    객체지향

--- 06_Object/src/com/kh/chap01_Abstraction/model/vo/Student.java
+++ 06_Object/src/com/kh/chap01_Abstraction/model/vo/Student.java
@@ -14,6 +14,25 @@ package com.kh.chap01_Abstraction.model.vo;
  *  }
  */
 
+//학생을 추상화해서 만든 클래스
+public class Student {
+	
+	// 필드부
+	// 접근제한자 [예약어] 자료형 변수명;
+	
+	// * 접근제한자 : 여기에 접근할 수 있는 범위를 제한 할 수 있음
+	//			   (public > protected > default > private)
+	public String name;
+	public int age;
+	public double height;	
+	
+	
+	// 생성자부
+	
+	// 메소드부
+	
+
+}
 
 
 
